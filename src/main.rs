3dae50391996c77b32142e7da063219b93466f22
mitsu1119 <hachimitsu1119@gmail.com>
2023-09-03 02:57:43 +0900

    NIC に MTU を実装

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ mod nic;
 use nic::{ Nic, NicType };
 
 fn main() {
-    let mut nic = Nic::new("ttyAMA0".to_string(), NicType::ETHERNET, "/dev/ttyAMA0".to_string());
+    let mut nic = Nic::new("ttyAMA0".to_string(), NicType::ETHERNET, 16, "/dev/ttyAMA0".to_string());
 
     println!("{:?}", nic);
 
-    nic.transmit(&[1, 2, 3, 4, 5]);
+    let payload: &[u8] = &[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+    nic.transmit(payload);
 
     loop {
         println!("{:?}", nic.recv());
--- src/nic.rs
+++ src/nic.rs
@@ -6,14 +6,15 @@ use rppal::uart::{Uart, Parity, Error};
 pub struct Nic {
     name: String,
     nic_type: NicType,
-    port: Uart
+    port: Uart,
+    mtu: u16
 }
 
 #[allow(dead_code)]
 impl Nic {
     // NIC 構造体の作成
-    pub fn new(name: String, nic_type: NicType, serial_path: String) -> Self {
-        Nic { name, nic_type, port: Uart::with_path(serial_path, 9600, Parity::None, 8, 1).unwrap() }
+    pub fn new(name: String, nic_type: NicType, mtu: u16, serial_path: String) -> Self {
+        Nic { name, nic_type, port: Uart::with_path(serial_path, 115200, Parity::None, 8, 1).unwrap(), mtu }
     }
 
     // 別の NIC へバイト列を送信
@@ -22,8 +23,8 @@ impl Nic {
     }
 
     // 別の NIC からバイト列を受信
-    pub fn recv(&mut self) -> [u8; 10] {
-        let mut buf = [0u8; 10];
+    pub fn recv(&mut self) -> Vec<u8> {
+        let mut buf: Vec<u8> = vec![0u8; self.mtu as usize];
         self.port.read(&mut buf).expect("Could not recv");
         buf
     }
