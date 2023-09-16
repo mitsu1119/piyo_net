8cd21819885bf03e2d1396e11aaef53496df92ac
mitsu1119 <hachimitsu1119@gmail.com>
2023-09-17 05:55:45 +0900

    PiyoStack の独自エラー型を実装

--- src/piyostack.rs
+++ src/piyostack.rs
@@ -1,4 +1,24 @@
+use std::error;
+use std::fmt;
 use piyo_net::process::search_processes;
+
+type Result<T> = std::result::Result<T, PiyoStackError>;
+
+// PiyoStack のエラー型
+#[derive(Debug, Clone)]
+pub struct PiyoStackError;
+
+impl fmt::Display for PiyoStackError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PiyoStackError")
+    }
+}
+
+impl error::Error for PiyoStackError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
+}
 
 // piyonet のプロトコルスタック
 #[derive(Debug)]
@@ -9,8 +29,8 @@ impl PiyoStack {
         PiyoStack {}
     }
 
-    pub fn connect(&self) -> Result<(), i32> {
-        let Some(nics) = search_processes("piyo_nic") else { return Err(2); };
+    pub fn connect(&self) -> Result<()> {
+        let Some(nics) = search_processes("piyo_nic") else { return Err(PiyoStackError); };
         println!("{:?}", nics);
 
         Ok(())
